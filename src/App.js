83c63af17c14db1b776a9cd5165f62e29d9c4fbe
kubaszy <35173576+kubaszy@users.noreply.github.com>
2020-05-17 20:23:11 +0200

    Dodawanie obrazka + przeciaganie + wyswietlanie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
+      <ImageComponent></ImageComponent>
     </div>
   );
+};
+
+class ImageComponent extends React.Component {
+  state = {
+    altText: true,
+    file: null,
+  };
+
+  showImage = (event) => {
+    this.setState({
+      altText: false,
+      file: URL.createObjectURL(event.target.files[0]),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="Border">
+          {this.state.altText ? (
+            <>
+              <p>Dodaj Obrazek</p>
+              <input type="file" onChange={this.showImage} />
+            </>
+          ) : (
+            <img src={this.state.file} />
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
