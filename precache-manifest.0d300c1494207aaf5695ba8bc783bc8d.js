09117ce652c8d404c9b6c904a962022326ae3ce9
Sekru <rusekpawel1@gmail.com>
2020-05-19 18:15:38 +0000

    Updates

---
+++ precache-manifest.0d300c1494207aaf5695ba8bc783bc8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba965f598734a4a49a5c3d2e38d977cd",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "6ff2491cb68e604e609d",
+    "url": "/meme-generator/static/css/main.6e7685c5.chunk.css"
+  },
+  {
+    "revision": "2459fc4043548b980d4d",
+    "url": "/meme-generator/static/js/2.05925aa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.05925aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff2491cb68e604e609d",
+    "url": "/meme-generator/static/js/main.f175fe73.chunk.js"
+  },
+  {
+    "revision": "93c9e8ff07f6ae919938",
+    "url": "/meme-generator/static/js/runtime-main.ede517da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meme-generator/precache-manifest.8e4c137baa39f0819db6d71cf7e33a1b.js"
+  "/meme-generator/precache-manifest.0d300c1494207aaf5695ba8bc783bc8d.js"
 );
 
 self.addEventListener('message', (event) => {
